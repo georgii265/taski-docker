cdb74b75f46b94221a0d0e3dc2d0c4eeb5945248
Георгий Моргунов <georgii.morgunov@mail.ru>
2023-06-19 02:42:23 +0300

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,6 +9,9 @@ environ.Env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# read th .env file
+environ.Env.read_env(env_file=str(BASE_DIR) + '../.env')
+
 SECRET_KEY = env('SECRET_KEY')
 
 DEBUG = env('DEBUG', default=False)
